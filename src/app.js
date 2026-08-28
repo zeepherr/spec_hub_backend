03fe096bc750b1031d25df7174cf485e72003975
jaikw@JaiKwan <jaikwanpha@gmail.com>
2026-08-28 16:12:57 +0700

    addConfig

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
+import cors from "cors";
 import express from "express";
+import { corsOptions } from "./configs/index.js";
 import { errorHandler } from "./middlewares/error.middleware.js";
 import { notFound } from "./middlewares/notFound.middleware.js";
 import { requestMdw } from "./middlewares/request.middlewares.js";
 
 const app = express();
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(requestMdw);
 
--- src/configs/index.js
+++ src/configs/index.js
@@ -16,3 +16,11 @@ export const config = {
 
   client_url: process.env.CLIENT_URL || "http://localhost:5173",
 };
+
+export const corsOptions = {
+  origin: config.client_url, // Allow only this domain (e.g., your React Vite dev server)
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"], // Allowed HTTP methods
+  allowedHeaders: ["Content-Type", "Authorization"], // Allowed custom headers
+  optionsSuccessStatus: 200, // Legacy browser support
+  credentials: true,
+};
