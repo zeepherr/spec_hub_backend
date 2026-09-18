2b919c7932ba64187a00d2597738cfb606f1c14e
jaikw@JaiKwan <jaikwanpha@gmail.com>
2026-09-19 04:08:09 +0700

    fix cookie lax

--- src/utils/cookie.util.js
+++ src/utils/cookie.util.js
@@ -1,15 +1,16 @@
 import { config } from "../configs/index.js";
+const isProduction = config.node_env === "production";
 export const refreshCookieOptions = {
   httpOnly: true, //prevent document.cookie
-  sameSite: "lax",
-  secure: config.node_env === "production",
+  sameSite: isProduction ? "none" : "lax",
+  secure: isProduction,
   path: "/api/auth",
   maxAge: 7 * 24 * 60 * 60 * 1000,
 };
 
 export const clearRefreshCookieOptions = {
   httpOnly: true,
-  sameSite: "lax",
-  secure: config.node_env === "production",
+  sameSite: isProduction ? "none" : "lax",
+  secure: isProduction,
   path: "/api/auth",
 };
